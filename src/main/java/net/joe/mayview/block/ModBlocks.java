93f380639ffc381cac4fe017194df6dbfb7feb46
Recon1991 <kalanicook@yahoo.com>
2025-03-11 16:10:05 -1000

    updated fish_trap model and texture.  small edits to morphite_synthesizer texture

--- src/main/java/net/joe/mayview/block/ModBlocks.java
+++ src/main/java/net/joe/mayview/block/ModBlocks.java
@@ -18,14 +18,19 @@ public class ModBlocks {
 
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(Mayview.MOD_ID);
-    public static final DeferredBlock<Block> MORPHITE_ORE = registerBlock("morphite_ore", () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
-    public static final DeferredBlock<Block> DEEPSLATE_MORPHITE_ORE = registerBlock("deepslate_morphite_ore", () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
-    public static final DeferredBlock<Block> TECTONIC_ORE = registerBlock("tectonic_ore", () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
-    public static final DeferredBlock<Block> DEEPSLATE_TECTONIC_ORE = registerBlock("deepslate_tectonic_ore", () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> MORPHITE_ORE = registerBlock("morphite_ore",
+            () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> DEEPSLATE_MORPHITE_ORE = registerBlock("deepslate_morphite_ore",
+            () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> TECTONIC_ORE = registerBlock("tectonic_ore",
+            () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> DEEPSLATE_TECTONIC_ORE = registerBlock("deepslate_tectonic_ore",
+            () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops()));
+
     public static final DeferredBlock<Block> MORPHITE_SYNTHESIZER = registerBlock("morphite_synthesizer",
-            () -> new MorphiteSynthesizerBlock(BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops()));
+            () -> new MorphiteSynthesizerBlock(BlockBehaviour.Properties.of().noOcclusion().strength(3f).requiresCorrectToolForDrops()));
     public static final DeferredBlock<Block> FISH_TRAP = registerBlock("fish_trap",
-            () -> new FishTrapBlock(BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops()));
+            () -> new FishTrapBlock(BlockBehaviour.Properties.of().noOcclusion().strength(3f).requiresCorrectToolForDrops()));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
